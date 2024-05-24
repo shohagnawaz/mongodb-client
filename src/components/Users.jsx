5a3eeb22ed0b4e4069040efa709fe049e6b1dd12
shohag nawaz <snawazdigital@gmail.com>
2024-05-24 11:15:48 +0600

    deleteCom

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
     const handleDelete = (_id) => {
         console.log("delete", _id)
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: "DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+                alert("deleted successfully");
+                const remaining = users.filter(user => user._id !== _id);
+                setUsers(remaining); 
+            }
+        })
     }
     return (
         <div>
@@ -14,7 +29,9 @@ const Users = () => {
                         {user.name} : 
                         {user.email} :
                         {user._id}
-                        <button onClick={() => handleDelete(user._id)}>X</button></p>)
+                        <button 
+                            onClick={() => handleDelete(user._id)}
+                        >X</button></p>)
                     
                 }
             </div>
